ce0151be96672671dfe5160e2fd8e1a9a8ca2a6d
Eduardo Coyto Brignone <eduardocoyto@gmail.com>
2022-08-08 23:20:11 +0000

    actulizo

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def hello_flask():
 
 @app.route('/inicio')
 def show_home():
-    return render_template('Index.html')
+    return 'hello world'
 
 @app.route('/<string:country>/<string:variety>/<float:aroma>/<float:aftertaste>/<float:acidity>/<float:body>/<float:balance>/<float:moisture>')
 def result(country, variety, aroma, aftertaste, acidity, body, balance, moisture):
